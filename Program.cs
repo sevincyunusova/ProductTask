b148572b5f82e869314751234a6b2472034862d7
sevincyunusova <yunusovasevinc08@gmail.com>
2026-04-19 16:07:41 +0400

    details added

--- Program.cs
+++ Program.cs
@@ -1,27 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-namespace ProductManagement {
-    public class Product {
+namespace ProductManagement
+{
+    public class Product
+    {
         private string name;
         private string category;
         private double price;
         private DateTime productDate;
-        public string Name {
+        public string Name
+        {
             get { return name; }
             set { name = value; }
         }
-        public string Category {
+        public string Category
+        {
             get { return category; }
             set { category = value; }
         }
-        public double Price {
+        public double Price
+        {
             get { return price; }
             set { price = value; }
         }
-            public DateTime ProductDate {
+        public DateTime ProductDate
+        {
             get { return productDate; }
             set { productDate = value; }
         }
+        public Product(string name, string category, double price, DateTime date)
+        {
+            Name = name;
+            Category = category;
+            Price = price;
+            ProductDate = date;
+        }
     } 
-} 

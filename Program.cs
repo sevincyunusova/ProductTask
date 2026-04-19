c92eb3649fc519abbcb9a65d131fcef7ffb9c6ca
sevincyunusova <yunusovasevinc08@gmail.com>
2026-04-19 15:50:43 +0400

    product class created

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace ProductManagement {
+    public class Product {
+        private string name;
+        private string category;
+        private double price;
+        private DateTime productDate;
+    }
+}
